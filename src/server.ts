3cc84df51ceb9ce5a3df75a658616a1af16fdcb1
pablo-caparelli <pablocaparelli@gmail.com>
2025-10-09 10:10:58 -0300

    versión final server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 import router from "./router";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 import db from "./config/db";
 import colors from "colors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
+
+dotenv.config();
 
 //Conectar a base de datos
 export async function connectDB() {
@@ -16,10 +21,29 @@ export async function connectDB() {
   }
 }
 
+connectDB();
+
 const server = express();
 
+//Permitir conexiones
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else if (!origin) {
+      console.log("Permitir peticiones sin origen");
+      callback(null, true);
+    } else {
+      callback(new Error("Error de Cors"));
+    }
+  },
+};
+server.use(cors(corsOptions));
+
+//Leer datos de formularios
 server.use(express.json());
 
+server.use(morgan("dev"));
 server.use("/api/products", router);
 
 // server.get("/api", (req, res) => {
